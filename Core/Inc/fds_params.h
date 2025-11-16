5db976f74e2dbfd694ec6ccc698a4fd39f9b0d21
Jesse Cornman <ocanath@gmail.com>
2025-11-16 13:33:38 -0800

    had to pad filesystem or last setting gets overwritten - 64bit alignment on G431

--- Core/Inc/fds_params.h
+++ Core/Inc/fds_params.h
@@ -11,11 +11,12 @@
 
 typedef struct fds_params_t
 {
-	uint32_t module_number;	//zero-pad this so it's 32bit aligned, for now
+	uint32_t module_number;
 	uint32_t uart_baud_rate;
 	uint32_t fdcan_NBRP;
 	uint32_t fdcan_NTSEG1;
 	uint32_t fdcan_NTSEG2;
+	uint32_t unused_zeropad;	//fds structures on this processor must be 64-bit aligned or else the last value will get truncated due to integer  division underflow
 
 }fds_params_t;
 
